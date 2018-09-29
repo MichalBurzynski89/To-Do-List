75529dec76e62a41d1e51c4754c3c1aeb4e1b335
Michał Burzyński <michal.b89@wp.pl>
2018-09-29 18:31:03 +0200

    Added removeTask and showTasksAddedEarlier functions. The first one allows user removing specific task from list, the latter will generate tasks added during earlier session, it's based on localStorage.

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@ inputField.focus();
 const uncompletedList = document.querySelector('.bg-danger');
 const completedList = document.querySelector('.bg-success');
 
-const tasksList = {
+const tasksList = localStorage.getItem('To-Do List') ? JSON.parse(localStorage.getItem('To-Do List')) : {
     uncompleted: [],
     completed: []
 };
 
+showTasksAddedEarlier();
+
 document.getElementById('taskInputForm').addEventListener('submit', addTask);
 
 function addTask(e) {
+
     e.preventDefault();
     const taskContent = inputField.value;
     if (taskContent) {
@@ -19,12 +22,15 @@ function addTask(e) {
             uncompletedList.classList.remove('d-none');
         }
         addTaskToProperList(taskContent);
+        tasksList.uncompleted.push(taskContent);
+        localStorage.setItem('To-Do List', JSON.stringify(tasksList));
         inputField.value = '';
     }
     inputField.focus();
 }
 
 function addTaskToProperList(task, completed) {
+
     const list = completed ? completedList.childNodes[3] : uncompletedList.childNodes[3];
     const listItem = document.createElement('li');
     const buttons = document.createElement('span');
@@ -38,29 +44,57 @@ function addTaskToProperList(task, completed) {
     btnRemove.className = 'fas fa-trash-alt';
 
     btnComplete.addEventListener('click', addToCompleted);
-    // btnRemove.addEventListener('click', removeTask);
+    btnRemove.addEventListener('click', removeTask);
 
     buttons.appendChild(btnComplete);
     buttons.appendChild(btnRemove);
     listItem.appendChild(buttons);
     list.appendChild(listItem);
-
-    if (task === inputField.value) {
-        tasksList.uncompleted.push(task);
-        console.log(tasksList);
-    } else {
-        if (!tasksList.completed.length) completedList.classList.remove('d-none');
-        tasksList.uncompleted.splice(tasksList.uncompleted.indexOf(task), 1);
-        tasksList.completed.push(task);
-        console.log(tasksList);
-    }
 }
 
 function addToCompleted() {
+
     const task = this.parentNode.parentNode;
     if (task.parentNode.parentNode === uncompletedList) {
-        addTaskToProperList(task.textContent, true);
+        tasksList.uncompleted.splice(tasksList.uncompleted.indexOf(task.textContent), 1);
         uncompletedList.childNodes[3].removeChild(task);
         if (!tasksList.uncompleted.length) uncompletedList.classList.add('d-none');
+        if (!tasksList.completed.length) completedList.classList.remove('d-none');
+        addTaskToProperList(task.textContent, true);
+        tasksList.completed.push(task.textContent);
+    } else {
+        tasksList.completed.splice(tasksList.completed.indexOf(task.textContent), 1);
+        completedList.childNodes[3].removeChild(task);
+        if (!tasksList.completed.length) completedList.classList.add('d-none');
+        if (!tasksList.uncompleted.length) uncompletedList.classList.remove('d-none');
+        addTaskToProperList(task.textContent);
+        tasksList.uncompleted.push(task.textContent);
     }
+    localStorage.setItem('To-Do List', JSON.stringify(tasksList));
+}
+
+function removeTask() {
+
+    const task = this.parentNode.parentNode;
+    if (task.parentNode.parentNode === uncompletedList) {
+        tasksList.uncompleted.splice(tasksList.uncompleted.indexOf(task.textContent), 1);
+        uncompletedList.childNodes[3].removeChild(task);
+        if (!tasksList.uncompleted.length) uncompletedList.classList.add('d-none');
+    } else {
+        tasksList.completed.splice(tasksList.completed.indexOf(task.textContent), 1);
+        completedList.childNodes[3].removeChild(task);
+        if (!tasksList.completed.length) completedList.classList.add('d-none');
+    }
+    localStorage.setItem('To-Do List', JSON.stringify(tasksList));
+}
+
+function showTasksAddedEarlier() {
+
+    if (!tasksList.uncompleted.length && !tasksList.completed.length) return;
+
+    if (tasksList.uncompleted.length) uncompletedList.classList.remove('d-none');
+    if (tasksList.completed.length) completedList.classList.remove('d-none');
+
+    tasksList.uncompleted.forEach(task => addTaskToProperList(task));
+    tasksList.completed.forEach(task => addTaskToProperList(task, true));
 }
